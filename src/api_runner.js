cd7964cf844741d2a2ab5c99f242c6dd16fd4e4e
amarantino159 <amarantino159@gmail.com>
2025-07-19 10:57:17 -0700

    refacrtored so constructors work with fetches

--- src/api_runner.js
+++ src/api_runner.js
@@ -1,38 +1,60 @@
 class Scryfall_API_Fetcher{
 
-  constructor(){
+  constructor(data){
     this.api_start = 'https://api.scryfall.com/cards/search?';
 
+    this.data=data;
+    // this.data2;
+    // this.data = Scryfall_API_Fetcher.search_a_Card('defenestrate');
+    // console.log(this.data);
+    // this.search_a_Card(search);
   };
 
-  async search_a_Card(search){
-    let ans = await fetch(`${this.api_start}q=${search}`)
-    .then(rawResponse => {
-      if(!rawResponse.ok){
-        throw new Error(`code: ${rawResponse.status}, status text: ${rawResponse.statusText}`)
-      }
-      return rawResponse.json()
-    })
-    //.then(jsonifiedResponse => console.log("Jsonified data: ", jsonifiedResponse))
-    .catch( error => {throw new Error('JSON or API error')})//console.log(error))
-    //console.log('in the search_a_card func ',ans);
-    return ans;
+  static async create(search){
+    data = await fetch(`https://api.scryfall.com/cards/search?q=${search}`);
+    data = await data.json();
+    return new Scryfall_API_Fetcher(data);
+  }
+
+
+
+  static async search_a_Card(fetcher,search){
+    try{
+      var ans = await fetch(`https://api.scryfall.com/cards/search?q=${search}`);
+      data = await ans.json();
+      fetcher.data = await data;
+    }
+    catch(error){
+      console.log(error);
+    };
+    //fetcher.data = data;
   };
 
-  async get_first_card(search){
-    let full_call = await this.search_a_Card(search);
-    let full_data0 = full_call['data'][0];
-    // console.log(full_data[0])
-    return full_data0;
-  };
+  // static async loadcheck(){
+  //   try{
+  //     this.data != undefined
+  //   }
+  //   catch(error){
+  //     console.log(error);
+  //   };
+  //   return data;
+  // };
 
-  async get_first_image_of_card(search){
-    let card = this.get_first_card(search);
-    let card_image_obj = card['image_uris'];
-    let card_img_url = card_image_obj['small'];
-    console.log(card_img_url);
-    return card_img_url;
-  };
+
+  // async get_first_card(search){
+  //   let full_call = await this.search_a_Card(search);
+  //   let full_data0 = full_call['data'][0];
+  //   // console.log(full_data[0])
+  //   return full_data0;
+  // };
+
+  // async get_first_image_of_card(search){
+  //   let card = this.get_first_card(search);
+  //   let card_image_obj = card['image_uris'];
+  //   let card_img_url = card_image_obj['small'];
+  //   console.log(card_img_url);
+  //   return card_img_url;
+  // };
 
 }
 
--- test/api_runner.test.js
+++ test/api_runner.test.js
@@ -1,16 +1,108 @@
 const Scryfall_API_Fetcher = require('../src').Scryfall_API_Fetcher;
 beforeEach( ()=>{
-  //teacher = new Teacher('Jane Doe', 'Biology', 'UCLA', 6);
-  fetcher = new Scryfall_API_Fetcher();
+
+//   fetcher = new Scryfall_API_Fetcher('defenestrate');
+//   const temp = async () => {
+//     let x = await Scryfall_API_Fetcher.search_a_Card(fetcher,'defenestrate');
+//     // console.log(typeof x)
+//     while(x == undefined){
+//       setTimeout(()=>{},500)
+//       console.log("Promise!")
+//     }
+// setTimeout(()=>{},500);
+//     this.data2=15;
+//     return x;
+//   };
+//   temp();
+
+  doThis = async (search) =>{
+    fetcher = await Scryfall_API_Fetcher.create(search);
+    return fetcher;
+  };
+
 });
 
 describe('API Fetcher', () => {
   describe('insantiates a new fetcher class', () => {
     test('takes the following arguments:', () => {
-      //expect(grade.graduationYear).toBe(2028);
-      expect(typeof (fetcher)).toBe('object');
+
+      doThis('defenestrate')
+      .then((fetcher)=>{
+        expect(typeof (fetcher)).toBe('object');
+      });
+    });
+    test('fetcher has data:', () => {
+      //console.log(fetcher.data2)
+      doThis('defenestrate')
+      .then((fetcher)=>{
+              console.log(fetcher['data']['total_cards'])
+            })
+      .then((fetcher)=>{
+        expect(typeof fetcher['data']['total_cards']).toBe('number');
+        return fetcher;
+      });
     });
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   describe('search_a_Card(search)', () => {
     test('Function is function', () => {
@@ -35,21 +127,21 @@ describe('API Fetcher', () => {
       expect(typeof response).toBe('object');
     });
   });
-    describe('get_first_image_of_card(search)', () => {
-    test('Function is function', () => {
-      expect(typeof fetcher.get_first_image_of_card).toBe('function');
-    });
+  // describe('get_first_image_of_card(search)', () => {
+  //   test('Function is function', () => {
+  //     expect(typeof fetcher.get_first_image_of_card).toBe('function');
+  //   });
 
-    test('Function returns the needed thing\'s type', () => {
-      let response = fetcher.get_first_image_of_card('defenestrate');
-      // console.log(response);
-      expect(typeof response).toBe('string');
-    });
-    test('Function returns the needed thing', () => {
-      let response = fetcher.get_first_image_of_card('defenestrate');
-      // console.log(response);
-      expect(response).toBe('https://cards.scryfall.io/small/front/8/8/88ce5cb9-7a29-4e4c-8bdb-4d3847067b33.jpg?1673159485');
-    });
-  });
+  //   test('Function returns the needed thing\'s type', () => {
+  //     let response = fetcher.get_first_image_of_card('defenestrate');
+  //     // console.log(response);
+  //     expect(typeof response).toBe('string');
+  //   });
+  //   test('Function returns the needed thing', () => {
+  //     let response = fetcher.get_first_image_of_card('defenestrate');
+  //     // console.log(response);
+  //     expect(response).toBe('https://cards.scryfall.io/small/front/8/8/88ce5cb9-7a29-4e4c-8bdb-4d3847067b33.jpg?1673159485');
+  //   });
+  // });
 
 });
